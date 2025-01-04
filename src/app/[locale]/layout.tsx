04661e189d5ca06776a38374c50d51b8da0c35b3
lilinkw <kjlinh098@gmail.com>
2025-01-05 00:29:07 +0700

    fix: move layout to locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next";
 import { Playfair, Montserrat } from "next/font/google";
 import "./globals.css";
-import { getCurrentLocale } from "../../locales/server";
 import Header from "@/components/Header";
 import Providers from "@/providers/ThemeProvider";
 import { I18nProviderClient } from "@/locales/client";
+import { setStaticParamsLocale } from "next-international/server";
 
 const playFair = Playfair({
   variable: "--font-playfair",
@@ -16,12 +15,22 @@ const montserrat = Montserrat({
   subsets: ["latin"],
 });
 
+export async function generateStaticParams() {
+  // Add the locales you want to support
+  return ['en', 'vi'].map((locale) => ({
+    params: { locale }, // Specify locale as a parameter
+  }));
+}
+
 export default async function RootLayout({
+  params,
   children,
 }: Readonly<{
+  params: Promise<{ locale: string }>;
   children: React.ReactNode;
 }>) {
-  const locale = await getCurrentLocale();
+  const { locale } = await params;
+  setStaticParamsLocale(locale);
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={`${playFair.variable} ${montserrat.variable} `}>
